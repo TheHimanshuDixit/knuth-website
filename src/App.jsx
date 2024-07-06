39141709a0b33ef399ca245343f751d6c4c9cf78
Divya2306 <96518035+Divya2306@users.noreply.github.com>
2024-07-06 14:21:52 +0530

    new routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Signup from "./pages/signup.jsx";
+import Signup from "./pages/Signup.jsx";
+import Login from "./pages/Login.jsx";
+import Home from "./pages/Home.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    ),
+    element: <Home />,
   },
   {
     path: "signup",
     element: <Signup />,
   },
+  {
+    path:"login",
+    element: <Login />,
+  }
 ]);
 
 function App() {
