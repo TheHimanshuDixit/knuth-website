09b90d75f4f92a7f1288ce13b415e8a0558d46da
Divya <96518035+Divya2306@users.noreply.github.com>
2024-07-07 15:41:56 +0530

    more routes created (#6)

---
+++ src/pages/Announcements.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Announcements = () => {
+  return (
+    <div>
+      <h1>announce</h1>
+    </div>
+  )
+}
+
+export default Announcements
---
+++ src/pages/Connect.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Connect = () => {
+  return (
+    <div>
+      <h1>connect</h1>
+    </div>
+  )
+}
+
+export default Connect
